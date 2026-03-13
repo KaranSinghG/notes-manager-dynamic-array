1d21aadb79a0bb071b43c67fa35d21b4c41519f7
KaranSinghG <karansinghg@ymail.com>
2026-03-13 21:39:26 +0530

    Implement NotesRepository class with dynamic array for note storage

---
+++ NotesRepository.java
@@ -0,0 +1,28 @@
+public class NotesRepository {
+    private Note[] notes;
+    private int size;
+    private int capacity = 10;
+
+    public NotesRepository() {
+        this.notes = new Note[capacity];
+        this.size = 0;
+    }
+
+    public void addNote(Note note) {
+        if (size == capacity) {
+            resize();
+        }
+        notes[size] = note;
+        size++;
+    }
+
+    private void resize() {
+        capacity = capacity * 2;
+        Note[] newNotes = new Note[capacity];
+        for (int i = 0; i < size; i++) {
+            newNotes[i] = notes[i];
+        }
+        notes = newNotes;
+    }
+
+}
